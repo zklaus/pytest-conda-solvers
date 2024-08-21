8f3af940ed16f157ebf092fd32cc1c4cfaa6e3cf
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2024-08-21 17:29:04 +0200

    Add basic yaml handling

--- pytest_conda_solvers/plugin.py
+++ pytest_conda_solvers/plugin.py
@@ -1,4 +1,49 @@
 import pytest
+from ruamel.yaml import YAML
+
+
+def pytest_collect_file(parent, file_path):
+    if file_path.suffix == ".yaml":
+        return YamlFile.from_parent(parent, path=file_path)
+
+
+class YamlFile(pytest.File):
+    def collect(self):
+        yaml = YAML(typ="safe")
+        raw = yaml.load(self.path.open(encoding="utf-8"))
+        for name, spec in sorted(raw.items()):
+            yield YamlItem.from_parent(self, name=name, spec=spec)
+
+
+class YamlItem(pytest.Item):
+    def __init__(self, *, spec, **kwargs):
+        super().__init__(**kwargs)
+        self.spec = spec
+
+    def runtest(self):
+        for name, value in sorted(self.spec.items()):
+            # Some custom test execution (dumb example follows).
+            if name != value:
+                raise YamlException(self, name, value)
+
+    def repr_failure(self, excinfo):
+        """Called when self.runtest() raises an exception."""
+        if isinstance(excinfo.value, YamlException):
+            return "\n".join(
+                [
+                    "usecase execution failed",
+                    "   spec failed: {1!r}: {2!r}".format(*excinfo.value.args),
+                    "   no further details known at this point.",
+                ]
+            )
+        return super().repr_failure(excinfo)
+
+    def reportinfo(self):
+        return self.path, 0, f"usecase: {self.name}"
+
+
+class YamlException(Exception):
+    """Custom exception for error reporting."""
 
 
 def pytest_addoption(parser):
