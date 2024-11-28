90eba7cf75f03b2ddc8f6f9b5e9d30d957e6592a
Klaus Zimmermann <klaus.zimmermann@quansight.com>
2024-11-28 08:40:17 +0100

    Move first test case to full templating

--- pytest_conda_solvers/base_tests/install.py
+++ pytest_conda_solvers/base_tests/install.py
@@ -9,7 +9,6 @@ from conda.core.prefix_data import PrefixData
 from conda.history import History
 from conda.models.channel import Channel
 from conda.models.records import PackageRecord, PrefixRecord
-from conda.resolve import MatchSpec
 
 from ..server import ChannelServer
 
@@ -19,7 +18,7 @@ def get_solver(
     solver_backend,
     tmpdir,
     channel_server,
-    channel_names,
+    channels,
     subdirs,
     specs_to_add=(),
     specs_to_remove=(),
@@ -28,7 +27,7 @@ def get_solver(
 ):
     channels = [
         Channel(channel_server.get_channel_url(channel_name))
-        for channel_name in channel_names
+        for channel_name in channels
     ]
     tmpdir = tmpdir.strpath
     pd = PrefixData(tmpdir)
@@ -51,6 +50,20 @@ def convert_to_dist_str(state: IndexedSet[PackageRecord]) -> IndexedSet[str]:
     return IndexedSet(prec.dist_str() for prec in state)
 
 
+def ensure_list(entry):
+    if isinstance(entry, str):
+        return [entry]
+    if isinstance(entry, list):
+        return [str(e) for e in entry]
+    return [str(entry)]
+
+
+def add_base_url(base_url, arch, dist_strs):
+    return type(dist_strs)(
+        f"{base_url}/{dist_str.replace('${{ arch }}', arch)}" for dist_str in dist_strs
+    )
+
+
 class TestBasic:
     @pytest.mark.conda_solver_test
     def test_empty(self, env, test, channel_server: ChannelServer):
@@ -60,51 +73,43 @@ class TestBasic:
 
     @pytest.mark.conda_solver_test
     def test_solve(self, env, tmpdir, solver_backend, test, channel_server):
-        specs = (MatchSpec("numpy"),)
-
+        input = test["input"]
         with get_solver(
             solver_backend,
             tmpdir,
             channel_server,
-            ["channel-1"],
-            ["linux-64"],
-            specs,
+            channels=ensure_list(input["channels"]),
+            subdirs=ensure_list(input["subdirs"]),
+            specs_to_add=ensure_list(input["specs_to_add"]),
         ) as solver:
             final_state = solver.solve_final_state()
-            prefix = f"{channel_server.get_channel_url("channel-1")}/linux-64"
-            order = (
-                f"{prefix}::openssl-1.0.1c-0",
-                f"{prefix}::readline-6.2-0",
-                f"{prefix}::sqlite-3.7.13-0",
-                f"{prefix}::system-5.8-1",
-                f"{prefix}::tk-8.5.13-0",
-                f"{prefix}::zlib-1.2.7-0",
-                f"{prefix}::python-3.3.2-0",
-                f"{prefix}::numpy-1.7.1-py33_0",
-            )
-            assert convert_to_dist_str(final_state) == order
 
-        specs_to_add = (MatchSpec("python=2"),)
-        with get_solver(
-            solver_backend,
-            tmpdir,
-            channel_server,
-            ["channel-1"],
-            ["linux-64"],
-            specs_to_add=specs_to_add,
-            prefix_records=final_state,
-            history_specs=specs,
-        ) as solver:
-            final_state = solver.solve_final_state()
-            prefix = f"{channel_server.get_channel_url("channel-1")}/linux-64"
-            order = (
-                f"{prefix}::openssl-1.0.1c-0",
-                f"{prefix}::readline-6.2-0",
-                f"{prefix}::sqlite-3.7.13-0",
-                f"{prefix}::system-5.8-1",
-                f"{prefix}::tk-8.5.13-0",
-                f"{prefix}::zlib-1.2.7-0",
-                f"{prefix}::python-2.7.5-0",
-                f"{prefix}::numpy-1.7.1-py27_0",
-            )
-            assert convert_to_dist_str(final_state) == order
+        ref = add_base_url(
+            channel_server.get_base_url(), "linux-64", test["output"]["final_state"]
+        )
+        assert convert_to_dist_str(final_state) == ref
+
+        # specs_to_add = (MatchSpec("python=2"),)
+        # with get_solver(
+        #     solver_backend,
+        #     tmpdir,
+        #     channel_server,
+        #     ["channel-1"],
+        #     ["linux-64"],
+        #     specs_to_add=specs_to_add,
+        #     prefix_records=final_state,
+        #     history_specs=specs,
+        # ) as solver:
+        #     final_state = solver.solve_final_state()
+        #     prefix = f"{channel_server.get_channel_url("channel-1")}/linux-64"
+        #     order = (
+        #         f"{prefix}::openssl-1.0.1c-0",
+        #         f"{prefix}::readline-6.2-0",
+        #         f"{prefix}::sqlite-3.7.13-0",
+        #         f"{prefix}::system-5.8-1",
+        #         f"{prefix}::tk-8.5.13-0",
+        #         f"{prefix}::zlib-1.2.7-0",
+        #         f"{prefix}::python-2.7.5-0",
+        #         f"{prefix}::numpy-1.7.1-py27_0",
+        #     )
+        #     assert convert_to_dist_str(final_state) == order
